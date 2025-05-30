0ad0910558997fdf6193be838e6378012a1ae92f
hawaricracker <63854180+hawaricracker@users.noreply.github.com>
2025-05-30 17:12:04 +0700

    Increase initial zombie count from 10 to 100 in main module

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ pygame.display.set_caption("Gen-Z Slayer")
 menu = MainMenu(WIDTH, HEIGHT)
 game_running = False
 run = True
-num_zombies = 10
+num_zombies = 100
 game_over = False
 victory = False
 
